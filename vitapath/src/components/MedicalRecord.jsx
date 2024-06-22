07419b46e97175dcd3c42aa75fde76d2a6180375
OliverAtondo <atondooliver17@hotmail.com>
2024-06-21 17:51:27 -0700

    VITAPATH-210624: API consumption
    API address problem solved.

--- vitapath/src/components/MedicalRecord.jsx
+++ vitapath/src/components/MedicalRecord.jsx
@@ -1,8 +1,26 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import MedicalRecordCard from './MedicalRecordCard'
+import MedicalRecordCard from './MedicalRecordCard';
 
 export default function MedicalRecord() {
+  const [patients, setPatients] = React.useState([]);
+
+  React.useEffect(() => {
+    async function fetchPatients() {
+      try {
+        const response = await fetch("https://api.react.vita-path.com/api/get-users");
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setPatients(data);
+      } catch (error) {
+        console.error('Failed to fetch patients:', error);
+      }
+    }
+    fetchPatients();
+  }, []);
+
   return (
     <Box
       sx={{
@@ -13,20 +31,15 @@ export default function MedicalRecord() {
           width: '100%',
           height: '100%'
         },
-          pl: 10,
-          pr: 10
+        pl: 10,
+        pr: 10
       }}
     >
-    <h1 color='#FFFFF'>Medical Records</h1>
-    <h2 color='#353535' >Admin Database</h2>
-    <MedicalRecordCard/>
-    <MedicalRecordCard/>
-    <MedicalRecordCard/>
-    <MedicalRecordCard/>
-    <MedicalRecordCard/>
-    <MedicalRecordCard/>
-    <MedicalRecordCard/>
-    <MedicalRecordCard/>
+      <h1 style={{ color: '#FFFFFF' }}>Medical Records</h1>
+      <h2 style={{ color: '#353535' }}>Admin Database</h2>
+      {patients.map((patient) => (
+        <MedicalRecordCard key={patient.id} patient={patient} />
+      ))}
     </Box>
   );
 }
--- vitapath/src/components/MedicalRecordCard.jsx
+++ vitapath/src/components/MedicalRecordCard.jsx
@@ -2,7 +2,13 @@ import React, { useState } from 'react';
 import { Dialog, DialogContent, DialogTitle, Typography, IconButton, Avatar, Grid, Card, CardContent, CardMedia } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
-function MedicalRecordPopup({ open, onClose }) {
+function MedicalRecordPopup({ open, onClose, patient }) {
+  // Directly accessing properties, assuming they are singular objects now
+  const personalInfo = patient.userDetails.personalInformation[0];
+  const medicalHistory = patient.userDetails.medicalHistory[0];
+  const vitalStats = patient.userDetails.vitalStatistics[0];
+  const digitalDocumentsLinks = patient.userDetails.digitalDocumentsLinks[0];
+
   return (
     <Dialog open={open} onClose={onClose} fullWidth maxWidth="md" sx={{ '& .MuiDialog-paper': { borderRadius: 2 } }}>
       <DialogTitle sx={{ m: 0, p: 2 }}>
@@ -20,29 +26,26 @@ function MedicalRecordPopup({ open, onClose }) {
         <Grid container spacing={2}>
           <Grid item xs={12} md={6} sx={{ display: 'flex', flexDirection: 'column' }}>
             <Avatar
-              alt="Robert Fox"
-              src="path_to_robert_fox_avatar.jpg" // Reemplaza con el path de la imagen real
+              alt={personalInfo.fullName}
+              src={personalInfo.profilePictureURL}
               sx={{ width: 90, height: 90, mb: 2 }}
             />
             <Typography variant="h4" gutterBottom component="div">
-              Robert Fox
+              {personalInfo.fullName}
             </Typography>
             <Typography variant="subtitle1" gutterBottom>
-              Male, 24
+              {personalInfo.gender}, {personalInfo.age}
             </Typography>
             <Typography variant="subtitle1" gutterBottom sx={{ color: 'secondary.main' }}>
-              Asthmatic Patient
+              Chronic Diseases: {medicalHistory.chronicDiseases}
             </Typography>
-            {/* Agrega los detalles adicionales como Allergies, Medications, etc. */}
-            
-            {/* Asumiendo que tienes imágenes para la sección de Medical Record, puedes utilizar CardMedia */}
             <Typography variant="h6" gutterBottom component="div" sx={{ mt: 4 }}>
               Medical record
             </Typography>
             <Card>
               <CardMedia
                 component="img"
-                image="path_to_medical_record_image.jpg" // Reemplaza con el path de la imagen real
+                image={digitalDocumentsLinks.medicalRecordFilesFiles}
                 alt="Medical Record"
               />
             </Card>
@@ -51,22 +54,28 @@ function MedicalRecordPopup({ open, onClose }) {
             <Typography variant="h4" gutterBottom>
               Vital Signs
             </Typography>
-            {/* Agrega los detalles para Vital Signs */}
             <Typography variant="body1" sx={{ mb: 1 }}>
-              Blood Type: A+
+              Blood Type: {vitalStats.bloodType}
             </Typography>
-            {/* Repite la estructura para Height & Weight y BMI */}
-            
-            {/* Repite la estructura similar para Immunization Records y Medical History */}
+            <Typography variant="body1" sx={{ mb: 1 }}>
+              Weight: {vitalStats.weight} kg
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 1 }}>
+              Height: {vitalStats.height} cm
+            </Typography>
             <Typography variant="h6" gutterBottom component="div" sx={{ mt: 4 }}>
               Immunization Records
             </Typography>
-            {/* Agrega contenido para Immunization Records */}
-            
+            <a href={digitalDocumentsLinks.immunizationFilesFiles} target="_blank" rel="noopener noreferrer">View Immunization Record</a>
             <Typography variant="h6" gutterBottom component="div" sx={{ mt: 4 }}>
               Medical History
             </Typography>
-            {/* Agrega contenido para Medical History */}
+            <Typography variant="body1">
+              Chronic Diseases: {medicalHistory.chronicDiseases}
+            </Typography>
+            <Typography variant="body1">
+              Known Allergies: {medicalHistory.knownAllergies}
+            </Typography>
           </Grid>
         </Grid>
       </DialogContent>
@@ -75,62 +84,56 @@ function MedicalRecordPopup({ open, onClose }) {
 }
 
 
-function MedicalRecordCard() {
-    const [openPopup, setOpenPopup] = useState(false);
 
-    const handleOpenPopup = () => {
-        setOpenPopup(true);
-    };
+function MedicalRecordCard({ patient }) {
+  const [openPopup, setOpenPopup] = useState(false);
 
-    const handleClosePopup = () => {
-        setOpenPopup(false);
-    };
-    
+  const handleOpenPopup = () => {
+      setOpenPopup(true);
+  };
+
+  const handleClosePopup = () => {
+      setOpenPopup(false);
+  };
+  
   return (
-    <>
-    <Card sx={{
-        maxWidth: 345,
-        borderRadius: 1,
-        boxShadow: 1,
-        textAlign: 'left',
-        p: 2,
-        position: 'relative',
-        cursor: 'pointer'
-    }}
-    onClick={handleOpenPopup}>
-      <Avatar
-        alt="Cody Fisher"
-        src="path_to_avatar_image.jpg"
-        sx={{
-          width: 56,
-          height: 56,
-          position: 'absolute',
-          top: 16,
-          right: 16,
-        }}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div" sx={{ fontSize: 22, fontWeight: 'bold' }}>
-          Cody Fisher
-        </Typography>
-        <Typography variant="body2" color="text.secondary" sx={{ fontSize: 14, mb: 2 }}>
-          Male, 24
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Hipertension
-        </Typography>
-        <div>
-          <Typography variant="body2" component="p">
-            Allergies: [Allergy 1], [Allergy 2], ...
-          </Typography>
-          <Typography variant="body2" component="p">
-            Current Medications: [Medication 1], [Medication 2], ...
-          </Typography>
-        </div>
-      </CardContent>
-    </Card>
-    <MedicalRecordPopup open={openPopup} onClose={handleClosePopup} />
-    </>
+      <>
+          <Card sx={{
+              maxWidth: 345,
+              borderRadius: 1,
+              boxShadow: 1,
+              textAlign: 'left',
+              p: 2,
+              position: 'relative',
+              cursor: 'pointer'
+          }}
+          onClick={handleOpenPopup}>
+              <Avatar
+                  alt={patient.userDetails.personalInformation[0].fullName}
+                  src={patient.userDetails.personalInformation[0].profilePictureURL}
+                  sx={{
+                      width: 56,
+                      height: 56,
+                      position: 'absolute',
+                      top: 16,
+                      right: 16,
+                  }}
+              />
+              <CardContent>
+                  <Typography gutterBottom variant="h5" component="div" sx={{ fontSize: 22, fontWeight: 'bold' }}>
+                      {patient.userDetails.personalInformation[0].fullName}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary" sx={{ fontSize: 14, mb: 2 }}>
+                      {patient.userDetails.personalInformation[0].gender}, {patient.userDetails.personalInformation[0].age}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                      {patient.userDetails.medicalHistory[0].chronicDiseases}
+                  </Typography>
+                  {/* Additional patient details */}
+              </CardContent>
+          </Card>
+          <MedicalRecordPopup open={openPopup} onClose={handleClosePopup} patient={patient} />
+      </>
   );
 }
 
